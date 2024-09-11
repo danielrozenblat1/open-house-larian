19b99346c8bcd8139eb3a6f154a75525b486b927
Your Name <you@example.com>
2024-09-11 10:50:28 +0300

    addedNav

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 import FirstScreen from './screens/FirstScreen';
 import ThirdScreen from './screens/ThirdScreen';
 import SecondScreen from './screens/SecondScreen';
+import NavBarNew from './components/NewNav/NavBarNew';
 
 function App() {
   return <>
+  <NavBarNew/>
   <FirstScreen/>
   <ThirdScreen/>
   <SecondScreen/>
